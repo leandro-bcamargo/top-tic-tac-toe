f3bed1e78a0b3f8e33897453f33f2d4825e0b8e1
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-08-02 10:17:00 -0300

    More auxiliary functions and progress towards implementing the game on console

--- src/script.js
+++ src/script.js
@@ -1,18 +1,19 @@
 
 const gameboard = ['','','','','','','','',''];
 
-function getFirstPlayer(firstPlayer) {
-  if (firstPlayer == 'X') {
-    playerX.isMyTurn = true;
-    playerO.isMyTurn = false;
-  } else if (firstPlayer == 'O') {
-    playerO.isMyTurn = true;
-    playerX.isMyTurn = false;
-  }
+let round = 1;
+let firstPlayer = getFirstPlayer();
+let currentPlayer = round === 1 ? getFirstPlayer() : getNextPlayer();
+
+function getFirstPlayer() {
+  const firstPlayer = prompt("Who should play first, X or O?");
+  if (firstPlayer !== "X" && firstPlayer !== "O") throw new Error('Invalid choice; you should pick either X or O');
+
+  return firstPlayer;
 }
 
 function updateGameboard(position) {
-  gameboard[position] = playerO.isMyTurn ? '0' : 'X';
+  gameboard[position] = currentPlayer === 'O' ? 'O' : 'X';
 }
 
 function getPosition() {
@@ -20,13 +21,40 @@ function getPosition() {
   const position = prompt("What position would you like to create a mark on?");
   if (!(availablePositions.includes(position.toString))) throw new Error('Invalid position. Please enter a number between 1 and 9');
   if (gameboard[position]) throw new Error('That position has already been marked. Please pick another position');
-  return parseInt(position);
+  return parseInt(position) - 1;
+}
+
+function getNextPlayer() {
+  return currentPlayer === 'O' ? 'X' : 'O';
+}
+
+function updateRound() {
+  round++;
+}
+
+function checkIfGameOver() {
+
+}
+
+function checkForWins() {
+  const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+  ];
+
+  for (let winningCombo of winningCombos) {
+    if (winningCombo.every(position => gameboard[position] === currentPlayer)) return true;
+  }
+
+  return false;
 }
 
 function playGame(firstPlayer) {
-  setFirstPlayer(firstPlayer);
   const position = getPosition();
   updateGameboard(position);
+  updateRound();
+  checkIfGameOver();
 }
 
 const playerX = {
