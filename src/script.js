9b695246c2416fc71375d580498a144f1246d456
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-08-10 15:47:50 -0300

    Implement checkWin function

--- src/script.js
+++ src/script.js
@@ -6,8 +6,27 @@ cells.forEach(cell => {
   cell.addEventListener('click', handleClick, {once: true})
 })
 
+function checkWin(player) {
+  const winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
-function handleClick(e) {
+  return winPatterns.some(pattern => {
+    return pattern.every(index => {
+      console.log("cells[index]:", cells[index])
+      return cells[index].textContent === player;
+    })
+  })
+}
+
+function resetBoard() {
 
 }
 
+
+function handleClick(e) {
+  const cell = e.target;
+  cell.textContent = currentPlayer;
+  if (checkWin(currentPlayer)) {
+    alert(`${currentPlayer} wins!`);
+
+  }
+}
