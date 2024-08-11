8c709ef69de4f53248afabc81377067e9c672abc
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-08-11 17:25:04 -0300

    Create setUpBoard, checkDraw, finish handleClick. Not yet working

--- src/script.js
+++ src/script.js
@@ -1,32 +1,58 @@
 const cells = document.querySelectorAll("[data-cell]");
 
-let currentPlayer = 'X';
+let currentPlayer;
 
-cells.forEach(cell => {
-  cell.addEventListener('click', handleClick, {once: true})
-})
+function setStartingPlayer() {
+  let startingPlayer = prompt("Who should play first, X or O?").toUpperCase();
+  while (startingPlayer !== "X" && startingPlayer !== "O") {
+    startingPlayer = prompt("Invalid choice. Please select either X or O").toUpperCase();
+  }
+  currentPlayer = startingPlayer;
+}
 
 function checkWin(player) {
   const winPatterns = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
 
   return winPatterns.some(pattern => {
     return pattern.every(index => {
-      console.log("cells[index]:", cells[index])
       return cells[index].textContent === player;
     })
   })
 }
 
-function resetBoard() {
-
+function setUpBoard() {
+  cells.forEach(cell => {
+    cell.textContent = "";
+    cell.addEventListener('click', handleClick, {once: true}
+    )
+  })
+  setStartingPlayer();
 }
 
+function checkDraw() {
+  console.log("cells:", cells);
+  return cells.every(cell => cell.textContent);
+}
+
+function switchCurrentPlayer() {
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
+}
 
 function handleClick(e) {
   const cell = e.target;
   cell.textContent = currentPlayer;
   if (checkWin(currentPlayer)) {
     alert(`${currentPlayer} wins!`);
-
+    setUpBoard();
+  } else if (checkDraw()) {
+    alert("It's a draw!")
+    setUpBoard();
+  } else {
+    console.log("trocou")
+    switchCurrentPlayer();
   }
 }
+
+(() => {
+  setUpBoard();
+})();
